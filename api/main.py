1245953a811a80d24c2ef1fd6e56f845d0fe3ace
Jenloke Magbojos <mjenloke@gmail.com>
2025-04-02 22:52:00 +0800

    post request body for main problem, basemodel class problem

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
+
+class Problem(BaseModel):
+  weight: List[int]
+  value: List[int]
+  capacity: int
+  algo: str
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
+@app.post("/")
+def read_root(data: Problem):
+  # insert algo
+  return sum(data.value)
