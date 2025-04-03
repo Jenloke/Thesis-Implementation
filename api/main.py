48352c6464b8fb32bfbdf4a7929c327bef1a94cc
Jenloke Magbojos <mjenloke@gmail.com>
2025-04-03 11:17:14 +0800

    add ships and containers endpoint for implementation

--- api/main.py
+++ api/main.py
@@ -14,3 +14,11 @@ app = FastAPI()
 def read_root(data: Problem):
   # insert algo
   return sum(data.value)
+
+@app.get("/ships")
+def get_ships():
+  return [100, 200]
+
+@app.get("/containers")
+def get_containers():
+  return [[100, 100], [200, 200]]
