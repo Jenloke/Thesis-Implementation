bdb915b5f18d95468eb0353fa5d53c45faccd828
Jenloke <mjenloke@gmail.com>
2025-04-10 20:42:29 +0800

    finsished container or problem func

--- api/dataset.py
+++ api/dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Returns list of ships
-def data_ships():
+def data_ships(l):
   df_ships = pd.read_csv('dataset/Ship_Performance_Dataset.csv')
   df_ships.dropna(inplace=True)
   # df_ships.info()
@@ -15,10 +15,14 @@ def data_ships():
     {'index': 4, 'max_weight': 1000},
   ]
 
-def data_containers():
-  return [
-    {'index': 0, 'value': 100, 'weight': 100},
-    {'index': 1, 'value': 200, 'weight': 200},
-    {'index': 2, 'value': 300, 'weight': 300},
-    {'index': 3, 'value': 400, 'weight': 400},
-  ]
+def data_containers(length: int):
+  df_containers = pd.read_csv('dataset/ph_commodity_trade_statistics_data.csv')
+  df_containers.dropna(inplace=True)
+  
+  df_containers_sample = df_containers.sample(length, random_state=100, ignore_index=True)
+  df_containers_sample = df_containers_sample[['trade_usd', 'weight_kg']]
+  
+  df_containers_sample.rename(columns={'trade_usd' : 'value', 'weight_kg' : 'weight'}, inplace=True)
+  df_containers_sample['weight'] = df_containers_sample['weight'].round()
+
+  return df_containers_sample.to_dict('records')
