2879342d13bd60785b85d73e598da8b3fd336ccf
Jenloke <mjenloke@gmail.com>
2025-04-10 22:04:28 +0800

    medj final api request, for cleanup

--- api/main.py
+++ api/main.py
@@ -5,6 +5,10 @@ from pydantic import BaseModel
 from dataset import data_ships
 from dataset import data_containers
 
+from algo import hsa
+from algo import pso
+from algo import epso
+
 class Problem(BaseModel):
   # weight: List[int]
   # value: List[int]
@@ -17,28 +21,30 @@ app = FastAPI()
 # Solve Route
 @app.post("/") 
 def read_root(data: Problem):
-  # print(data)
+  # from input app: algo, capacity, length  
+
+  problem = data_containers(data.length)
+  # print(problem)
+  if data.algo == 'hsa':
+    solution = hsa(problem, data.capacity)
+  elif data.algo == 'pso':
+    solution = pso(problem, data.capacity)
+  elif data.algo == 'epso':
+    solution = epso(problem, data.capacity)
   
-  # from input app 
-  # algo
-  # ship capacity
-  # number of containers / problem length
-  
-  # from within api
-  # problem - sample from containers list usi\
+  return {'problem': problem, 'solution': solution}
   
   # returns
   # gathered data
   # solution
   # container value/profit
-  # total weight
-  
-  return data
+  # total weight 
+  # return data
 
 @app.get("/ships")
 def get_ships():
   return data_ships()
 
-@app.get("/containers")
-def get_containers():
-  return data_containers()
+# @app.get("/containers")
+# def get_containers():
+#   return data_containers()
