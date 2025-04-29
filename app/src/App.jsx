996a3d082547abdef75fdfd436624490b1519b38
Jenloke Magbojos <mjenloke@gmail.com>
2025-04-29 22:14:19 +0800

    final app layout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,164 +5,308 @@ import './App.css'
 function App() {
   // Generated Data
   const [shipsCapacity, setShipsCapacity] = useState([])
+  const [problem, setProblem] = useState([])
 
   // Default Values
   const container_list = [100, 200, 500]
-  const algo = [
-    { label: 'HSA', value: 'hsa' },
-    { label: 'PSO', value: 'pso' },
-    { label: 'EPSO', value: 'epso' },
-  ]
+
+  // App State
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
   // Form Inputs
   const [selectedCapacity, setSelectedCapacity] = useState('')
+  const [selectedOpCost, setOpCost] = useState('')
   const [selectedContainer, setContainer] = useState('')
-  const [selectedAlgo, setAlgo] = useState('')
 
   // Solutions
-  const [problem, setProblem] = useState([])
+  const [problemAPI, setProblemAPI] = useState([])
   const [solution, setSolution] = useState({})
   const [solArray, setSolArray] = useState([])
   const [runtime, setRuntime] = useState(0)
+  // Base on Solved Solutions
+  const [finalOpCost, setfinalOpCost] = useState(0)
+  const [profit, setProfit] = useState(0)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('/api/ships')
-        console.log(response.data)
-        setShipsCapacity(response.data)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
-    }
-    fetchData()
-  }, [])
-
-  const handleSolve = async () => {
-    if (!selectedAlgo || !selectedCapacity || !selectedContainer) {
-      alert('Please Complete')
-      return
-    }
-    console.log(selectedCapacity, selectedContainer, selectedAlgo)
-    const capacity = selectedCapacity
-    const length = selectedContainer
-    const algo = selectedAlgo
-
+  const fetchShips = async () => {
     try {
-      const response = await axios.post('/api/', {
-        length: length,
-        capacity: capacity,
-        algo: algo, // hsa, pso, e-pso
-      })
-      console.log(response.data)
-      const data = response.data
-
-      setProblem(data.problem)
-
-      const { solArray, ...metrics } = data.solution
-      setSolArray(solArray)
-      setSolution(metrics)
-
-      setRuntime(data.runtime)
+      const response = await axios.get('/api/ships')
+      // console.log(response.data)
+      setShipsCapacity(response.data)
     } catch (error) {
       console.error('Error fetching data:', error)
     }
   }
 
+  const fetchContainers = async (length) => {
+    if (!selectedContainer) {
+      alert('Select Container Size')
+      return
+    }
+    try {
+      const response = await axios.post('/api/containers', {
+        length: length,
+      })
+      // console.log(response.data)
+      setProblem(response.data)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  useEffect(() => {
+    const fetchContainers = async (length) => {
+      try {
+        const response = await axios.post('/api/containers', {
+          length: length,
+        })
+        // console.log(response.data)
+        setProblem(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchShips()
+    fetchContainers(100)
+  }, [])
+
+  const handleSolve = async () => {
+    if (!selectedCapacity) {
+      alert('Please Complete')
+      return
+    }
+    if (!problem) {
+      alert('Container List Should not be Empty')
+    }
+    setIsSubmitting(true)
+
+    const finalOpCost = selectedOpCost
+    // console.log(selectedCapacity, selectedContainer)
+    const capacity = selectedCapacity
+
+    try {
+      const response = await axios.post('/api/', {
+        capacity: capacity,
+        problem: problem,
+      })
+      // console.log(response.data)
+      const data = response.data
+      const { solArray, ...metrics } = data.solution
+      setProblemAPI(data.problem)
+      setSolArray(solArray)
+      setSolution(metrics)
+      setfinalOpCost(finalOpCost)
+      setProfit(metrics.solValue - finalOpCost)
+      setRuntime(data.runtime)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  const clearAll = () => {
+    setShipsCapacity([])
+    setProblem([])
+
+    setIsSubmitting(false)
+
+    // Form Inputs
+    setSelectedCapacity('')
+    setContainer('')
+
+    // Solutions
+    setSolution({})
+    setSolArray([])
+    setRuntime(0)
+    setfinalOpCost(0)
+    setProfit(0)
+  }
+
   return (
-    <div className='m-5 p-0 flex flex-col gap-3'>
-      <p>Metaheuristic Algorithm</p>
-      <button className='w-1/2 border-1' onClick={() => handleSolve()}>
-        Solve
-      </button>
+    <div className="m-5 p-0 flex flex-col gap-3 font-[Verdana]">
+      <div className="flex place-content-center w-full gap-5">
+        <img src="assets/logo.svg" alt="icon" width={48} height={48} />
+        <p className="text-4xl">Meta-Logistics</p>
+      </div>
 
-      <fieldset className='flex flex-col gap-5'>
-        <legend>Available Ships</legend>
-        {shipsCapacity.map((ship) => (
-          <div key={ship.Cargo_Weight_tons} className='flex gap-1'>
-            <input
-              type='radio'
-              name='capacity'
-              id={`${ship.Cargo_Weight_tons}-weight`}
-              value={ship.Cargo_Weight_tons}
-              onChange={() => setSelectedCapacity(ship.Cargo_Weight_tons)}
-            />
-            <div className='flex flex-col'>
-              <p>Ship Type: {ship.Ship_Type}</p>
-              <p>Carrying Capacity: {ship.Cargo_Weight_tons} tons</p>
-              <p>Operating Cost: {ship.Operational_Cost_USD} USD</p>
-            </div>
+      <div className="flex gap-2">
+        <button
+          className="w-1/2 py-2 m-auto border-1 rounded-md text-xl"
+          onClick={() => handleSolve()}
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? 'Finding Optimal Cargo' : 'Optimize Cargo'}
+        </button>
+        <button
+          className="w-1/2 py-2 m-auto border-1 rounded-md text-xl"
+          onClick={() => clearAll()}
+          disabled={isSubmitting}
+        >
+          Clear All
+        </button>
+      </div>
+
+      <div className="w-full flex place-content-center gap-2">
+        <div className="w-1/2 flex flex-col gap-5 place-content-center">
+          <div className="w-full">
+            <button
+              className="w-full py-2 mx-auto border-1 rounded-md text-xl"
+              onClick={() => fetchContainers(selectedContainer)}
+            >
+              Gather Containers
+            </button>
+            <legend>Containers</legend>
+            <fieldset className="flex mx-auto gap-5">
+              {container_list.map((length) => (
+                <div key={length} className="flex gap-1">
+                  <input
+                    type="radio"
+                    name="containers"
+                    id={`${length}-container`}
+                    value={length}
+                    onChange={() => setContainer(length)}
+                  />
+                  <label>{length}</label>
+                </div>
+              ))}
+            </fieldset>
           </div>
-        ))}
-      </fieldset>
-
-      <fieldset className='flex gap-5'>
-        <legend>Containers</legend>
-        {container_list.map((length) => (
-          <div key={length} className='flex gap-1'>
-            <input
-              type='radio'
-              name='containers'
-              id={`${length}-container`}
-              value={length}
-              onChange={() => setContainer(length)}
-            />
-            <label>{length}</label>
-          </div>
-        ))}
-      </fieldset>
-
-      <fieldset className='flex gap-5'>
-        <legend>Algorithm</legend>
-        {algo.map(({ label, value }) => (
-          <div key={value} className='flex gap-1'>
-            <input
-              type='radio'
-              name='algo'
-              id={value}
-              value={value}
-              onChange={() => setAlgo(value)}
-            />
-            <label>{label}</label>
-          </div>
-        ))}
-      </fieldset>
-
-      {Object.keys(solution).length !== 0 ? (
-        <>
-          <div className='w-full max-w-md border border-gray-300 overflow-hidden'>
-            <div className='bg-gray-100 border-b border-gray-300'>
-              <table className='w-full'>
+          <div className="w-full max-w-md border border-gray-300 rounded-md overflow-hidden">
+            <div className="bg-gray-100 border-b border-gray-300">
+              <table className="w-full">
                 <thead>
                   <tr>
-                    <th className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider'>
+                    <th className="pt-3" colSpan={3}>
+                      Container List
+                    </th>
+                  </tr>
+                </thead>
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
                       Container
                     </th>
-                    <th className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider'>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
                       Value
                     </th>
-                    <th className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider'>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
                       Weight
                     </th>
                   </tr>
                 </thead>
               </table>
             </div>
-            <div className='h-64 overflow-y-auto'>
-              <table className='w-full'>
-                <tbody className='bg-white divide-y divide-gray-200'>
+            <div className="h-64 overflow-y-auto">
+              <table className="w-full">
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {problem.map(({ value, weight }, index) => (
+                    <tr key={index} className="hover:bg-gray-50">
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {index + 1}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        $ {value.toLocaleString()}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {weight.toLocaleString()} {weight == 1 ? 'ton' : 'tons'}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2 flex flex-col gap-5">
+          <button
+            className="w-full py-2 border-1 rounded-md text-xl"
+            onClick={() => fetchShips()}
+          >
+            Gather Available Ships
+          </button>
+          <fieldset className="flex flex-col gap-5">
+            <legend>Available Ships</legend>
+            {shipsCapacity.map((ship) => (
+              <div key={ship.Cargo_Weight_tons} className="flex gap-1">
+                <input
+                  type="radio"
+                  name="capacity"
+                  id={`${ship.Cargo_Weight_tons}-weight`}
+                  value={ship.Cargo_Weight_tons}
+                  onChange={() => {
+                    setSelectedCapacity(ship.Cargo_Weight_tons)
+                    setOpCost(ship.Operational_Cost_USD)
+                  }}
+                />
+                <div className="flex flex-col">
+                  <p>Ship Type: {ship.Ship_Type}</p>
+                  <p>
+                    Carrying Capacity: {ship.Cargo_Weight_tons.toLocaleString()}{' '}
+                    tons
+                  </p>
+                  <p>
+                    Operating Cost: {ship.Operational_Cost_USD.toLocaleString()}{' '}
+                    USD
+                  </p>
+                </div>
+              </div>
+            ))}
+          </fieldset>
+        </div>
+      </div>
+
+      {Object.keys(solution).length !== 0 ? (
+        <div className="flex justify-center gap-10">
+          <div className="flex flex-col justify-center text-right">
+            <p className="text-2xl">Cargo Information</p>
+            <p>Value: $ {solution.solValue.toLocaleString()}</p>
+            <p>Operating Cost: $ {finalOpCost.toLocaleString()}</p>
+            <p>Profit: $ {profit.toLocaleString()}</p>
+            <p>Weight: {solution.solWeight.toLocaleString()} tons</p>
+            <p>Time to Find: {runtime} secs</p>
+          </div>
+
+          <div className="w-full max-w-md border border-gray-300 rounded-md overflow-hidden">
+            <div className="bg-gray-100 border-b border-gray-300">
+              <table className="w-full">
+                <thead>
+                  <tr>
+                    <th className="pt-3" colSpan={3}>
+                      Selected Containers
+                    </th>
+                  </tr>
+                </thead>
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
+                      Container
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
+                      Value
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
+                      Weight
+                    </th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div className="h-64 overflow-y-auto">
+              <table className="w-full">
+                <tbody className="bg-white divide-y divide-gray-200">
                   {solArray.map((value, index) => (
-                    <tr key={index} className='hover:bg-gray-50'>
+                    <tr key={index} className="hover:bg-gray-50">
                       {value == 1 ? (
                         <>
-                          <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             {index + 1}
                           </td>
-                          <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-900'>
-                            {problem[index].value} USD
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                            $ {problemAPI[index].value.toLocaleString()}
                           </td>
-                          <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-900'>
-                            {problem[index].weight} kg
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                            {problemAPI[index].weight.toLocaleString()}{' '}
+                            {problemAPI[index].weight == 1 ? 'ton' : 'tons'}
                           </td>
                         </>
                       ) : null}
@@ -172,51 +316,7 @@ function App() {
               </table>
             </div>
           </div>
-          <div>
-            <p>Cargo List</p>
-            <p>Value: {solution.solValue} USD</p>
-            <p>Weight: {solution.solWeight} tons</p>
-            <p>Time to Find: {runtime} secs</p>
-          </div>
-          <div className='w-full max-w-md border border-gray-300 overflow-hidden'>
-            <div className='bg-gray-100 border-b border-gray-300'>
-              <table className='w-full'>
-                <thead>
-                  <tr>
-                    <th className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider'>
-                      Container
-                    </th>
-                    <th className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider'>
-                      Value
-                    </th>
-                    <th className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider'>
-                      Weight
-                    </th>
-                  </tr>
-                </thead>
-              </table>
-            </div>
-            <div className='h-64 overflow-y-auto'>
-              <table className='w-full'>
-                <tbody className='bg-white divide-y divide-gray-200'>
-                  {problem.map(({ value, weight }, index) => (
-                    <tr key={index} className='hover:bg-gray-50'>
-                      <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>
-                        {index + 1}
-                      </td>
-                      <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-900'>
-                        {value}
-                      </td>
-                      <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-900'>
-                        {weight} kg
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </>
+        </div>
       ) : null}
     </div>
   )
