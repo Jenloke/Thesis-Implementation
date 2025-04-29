43b12043a976f0d023f00b0ebede649b88c34533
Jenloke Magbojos <mjenloke@gmail.com>
2025-04-29 22:12:16 +0800

    final api

--- api/main.py
+++ api/main.py
@@ -1,60 +1,39 @@
 from fastapi import FastAPI
-# from typing import List
+from typing import List
 from pydantic import BaseModel
 
 from dataset import data_ships
 from dataset import data_containers
 
-from algo import hsa
-from algo import pso
 from algo import epso
 
 from datetime import datetime
 
 class Problem(BaseModel):
-  # weight: List[int]
-  # value: List[int]
-  length: int
   capacity: int
-  algo: str
+  problem: List[dict]
+
+class Containers(BaseModel):
+  length: int
 
 app = FastAPI()
 
 # Solve Route
 @app.post("/") 
 def read_root(data: Problem):
-  # from input app: algo, capacity, length  
-
-  problem = data_containers(data.length)
-  # print(problem)
-  
   start_time = datetime.now()
-  
-  if data.algo == 'hsa':
-    solution = hsa(problem, data.capacity)
-  elif data.algo == 'pso':
-    solution = pso(problem, data.capacity)
-  elif data.algo == 'epso':
-    solution = epso(problem, data.capacity)
-    
+
+  solution = epso(data.problem, data.capacity)
+
   end_time = datetime.now()
   runTime = (end_time - start_time).total_seconds()
   
-  return {'problem': problem, 'solution': solution, 'runtime': runTime}
-  
-  # returns
-  # gathered data
-  # solution
-  # container value/profit
-  # total weight 
-  # return data
+  return {'problem': data.problem, 'solution': solution, 'runtime': runTime}
 
 @app.get("/ships")
 def get_ships():
   return data_ships()
 
-@app.get("/containers")
-def get_containers():
-  x = data_containers()
-  print(x)
-  return x
+@app.post("/containers")
+def get_containers(data: Containers):
+  return data_containers(data.length)
